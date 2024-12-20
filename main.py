52cb6260ba2a489438bbacb4f8ddf107eb1e78c4
Chemodurovart <chemodurovart@gmail.com>
2024-12-20 13:22:57 +0300

    add class Family and inf loop

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-##from random import randint
-##from threading import Thread
+from random import randint
+from threading import Thread
+from time import sleep
 
 class Request:
     def __init__(self, family):
         self.family = family
 
-##class Family(Thread):
-##    def __init__(prio):
-##        self.prio = prio
-##
-##def familyFunc():
-##    while True:
-##        a = randint(3, 8)
-        
+class Family(Thread):
+    def __init__(self, prio):
+        super().__init__()
+        self.prio = prio
+
+    def run(self):
+        while True:
+            a = randint(3, 8)
+            sleep(a)
+            r = Request(self)
+            print(r)
+
+
+
+if __name__ == '__main__':
+    f = Family(1)
+    f.start()
+    f.join()
