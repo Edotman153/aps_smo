e606a7f21a38c95ea61a9766b201c4e6eb5759ca
Chemodurovart <chemodurovart@gmail.com>
2024-12-20 12:59:42 +0300

    add class Request

---
+++ main.py
@@ -0,0 +1,15 @@
+##from random import randint
+##from threading import Thread
+
+class Request:
+    def __init__(self, family):
+        self.family = family
+
+##class Family(Thread):
+##    def __init__(prio):
+##        self.prio = prio
+##
+##def familyFunc():
+##    while True:
+##        a = randint(3, 8)
+        
